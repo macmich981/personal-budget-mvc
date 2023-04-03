8c6ace4cc1c12e3d8b0739955681b7720fd2bcec
macmich981 <macmich@onet.eu>
2023-04-03 17:24:37 +0200

    Database setup in php code

--- App/Config.php
+++ App/Config.php
@@ -14,25 +14,25 @@ class Config
      * Database host
      * @var string
      */
-    const DB_HOST = 'your-database-host';
+    const DB_HOST = 'localhost';
 
     /**
      * Database name
      * @var string
      */
-    const DB_NAME = 'your-database-name';
+    const DB_NAME = 'personal_budget';
 
     /**
      * Database user
      * @var string
      */
-    const DB_USER = 'your-database-user';
+    const DB_USER = 'cms_www';
 
     /**
      * Database password
      * @var string
      */
-    const DB_PASSWORD = 'your-database-password';
+    const DB_PASSWORD = 'FR4Dyv7Ds_[6RvkZ';
 
     /**
      * Show or hide error messages on screen
