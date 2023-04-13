e1ef0215bff1ad31ecca82e3c96c8b6f9bdadfb1
macmich981 <macmich@onet.eu>
2023-04-14 01:01:09 +0200

    Added more aesthetic signup form client validation

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,47 @@
+$(document).ready(function() {
+    $('#formSignup').validate({
+            errorPlacement: function(error, element) {
+            if (element.attr("name") == "username") {
+                error.insertAfter(".nameErr");
+                error.css('color', 'red');
+            }
+            else if (element.attr("name") == "email") {
+                error.insertBefore(".emailErr");
+                error.css('color', 'red');
+            }
+            else if (element.attr("name") == "password") {
+                error.insertBefore(".passErr");
+                error.css('color', 'red');
+            }
+            else if (element.attr("name") == "password_confirmation") {
+                error.insertBefore(".confirmErr");
+                error.css('color', 'red');
+            }
+        },
+        rules: {
+            name: 'required',
+            email: {
+                required: true,
+                email: true,
+                remote: '/account/validate-email'
+            },
+            password: {
+                required: true,
+                minlength: 6,
+                validPassword: true
+            },
+            password_confirmation: {
+                equalTo: '#inputPassword'
+            }
+        },
+        messages: {
+            email: {
+                remote: 'Adres email jest już zajęty'
+            }
+        }
+    });
+});
+
 $.validator.addMethod('validPassword',
     function(value, element, param) {
         if (value != '') {
@@ -10,5 +54,25 @@ $.validator.addMethod('validPassword',
         }
         return true;
     },
-    //'Must contain at least one letter and one number'
+    'Musi zawierać co najmniej jedną literę i cyfrę'
 );
+
+jQuery.extend(jQuery.validator.messages, {
+    required: "Pole wymagane.",
+    remote: "Please fix this field.",
+    email: "Please enter a valid email address.",
+    url: "Please enter a valid URL.",
+    date: "Please enter a valid date.",
+    dateISO: "Please enter a valid date (ISO).",
+    number: "Please enter a valid number.",
+    digits: "Please enter only digits.",
+    creditcard: "Please enter a valid credit card number.",
+    equalTo: "Please enter the same value again.",
+    accept: "Please enter a value with a valid extension.",
+    maxlength: jQuery.validator.format("Please enter no more than {0} characters."),
+    minlength: jQuery.validator.format("Please enter at least {0} characters."),
+    rangelength: jQuery.validator.format("Please enter a value between {0} and {1} characters long."),
+    range: jQuery.validator.format("Please enter a value between {0} and {1}."),
+    max: jQuery.validator.format("Please enter a value less than or equal to {0}."),
+    min: jQuery.validator.format("Please enter a value greater than or equal to {0}.")
+});
