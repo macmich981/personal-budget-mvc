f4d3e18678a266e75295898bf0dc4894c0b9785e
macmich981 <macmich@onet.eu>
2023-04-13 10:09:36 +0200

    Added user model, modified signup controller

--- App/Controllers/Signup.php
+++ App/Controllers/Signup.php
@@ -1,12 +1,21 @@
 <?php
 
 namespace App\Controllers;
+
 use \Core\View;
+use App\Models\User;
 
 class Signup extends \Core\Controller {
 
     public function newAction() {
         View::renderTemplate('Signup/new.html');
+    }
+
+    public function createAction() {
+        $user = new User($_POST);
+        $user->save();
+
+        View::renderTemplate('Signup/success.html');
     }
 
 }
--- App/Models/User.php
+++ App/Models/User.php
@@ -4,23 +4,29 @@ namespace App\Models;
 
 use PDO;
 
-/**
- * Example user model
- *
- * PHP version 7.0
- */
-class User extends \Core\Model
-{
+#[\AllowDynamicProperties]
+class User extends \Core\Model {
 
-    /**
-     * Get all the users as an associative array
-     *
-     * @return array
-     */
-    public static function getAll()
-    {
-        $db = static::getDB();
-        $stmt = $db->query('SELECT id, name FROM users');
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    public function __construct($data) {
+        foreach ($data as $key => $value) {
+            $this->$key = $value;
+        }
     }
+
+    public function save() {
+        $password = password_hash($this->password, PASSWORD_DEFAULT);
+
+        $sql = 'INSERT INTO users (username, email, password)
+                VALUES (:username, :email, :password)';
+        
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindValue(':username', $this->username, PDO::PARAM_STR);
+        $stmt->bindValue(':email', $this->email, PDO::PARAM_STR);
+        $stmt->bindValue(':password', $password, PDO::PARAM_STR);
+
+        $stmt->execute();
+    }
+
 }
